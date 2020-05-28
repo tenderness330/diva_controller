3703b9d790ab7d41d7ee29b7004167c2f7e428e0
tenderness330 <misawork330@gmail.com>
2020-05-29 01:00:21 +0900

    commit

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ import Adafruit_MPR121.MPR121 as MPR121
 
 
 cap = MPR121.MPR121()
+cap2 = MPR121.MPR121()
 
+# 初期化
 if not cap.begin():
     print('Error initializing MPR121.  Check your wiring!')
     sys.exit(1)
+
+if not cap2.begin(0x5B):
+    print('Error initializing MPR121.  Check your wiring!')
+    sys.exit(1)
+
 
 LED_PIN=14
 
