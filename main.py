0283d8e2e8f62e0d0385477f2271afba28af8dfc
tenderness330 <misawork330@gmail.com>
2020-06-17 23:20:51 +0900

    version v1.0 release!

--- main.py
+++ main.py
@@ -1,147 +1,132 @@
 import sys
+import Adafruit_MPR121.MPR121 as MPR121
 import time
 import wiringpi as pi
 
-import Adafruit_MPR121.MPR121 as MPR121
-
-# 各変数初期化
 cap = MPR121.MPR121()
 cap2 = MPR121.MPR121()
-touch_count = 1
-now_touch = 0b000000000000000000000000
-before_touch = 0b0
-touch_ave = 0
-touch_ave_size = 0
-touch_ave2 = 0
-touch_ave_size2 = 0
-pin_bit = 0b1
+before1 = 0b0
+before2 = 0b0
 
-IPpoint1 = 0
-IPpoint2 = 0
-IPpoint1_vector = 0
-IPpoint2_vector = 0
+R1_PIN = 4
+R2_PIN = 17
+L1_PIN = 27
+L2_PIN = 22
 
-# 初期化
+pi.wiringPiSetupGpio()
+pi.pinMode (R1_PIN, pi.OUTPUT)
+pi.pinMode (R2_PIN, pi.OUTPUT)
+pi.pinMode (L1_PIN, pi.OUTPUT)
+pi.pinMode (L2_PIN, pi.OUTPUT)
+
+
+
 if not cap.begin():
     print('Error initializing MPR121.  Check your wiring!')
     sys.exit(1)
 
 if not cap2.begin(0x5B):
-
     print('Error initializing MPR121.  Check your wiring!')
     sys.exit(1)
 
-'''
-touchpad1 0000 0000 0000 1111 1111 1111
-touchpad2 1111 1111 1111 0000 0000 0000
-no touch  0000 0000 0000 0000 0000 0000
-'''
 
 while True:
-    current_touched = cap.touched() << 1 # 一番端もスライド判定できるように端に論理的隙間を作る
-    current_touched2 = cap2.touched() << 13 # 連続したビットにするために13ビットシフトする
-    current_touch_all = current_touched | current_touched2 # 何かしらのビットが立っていれば触っている判定になる
+    touch1 = cap.touched() << 1
+    touch2 = cap2.touched() << 1
+    
+    touch1 = touch1 & -touch1
+    touch2 = touch2 & -touch2
 
 
-    if current_touch_all:
-        i = 1
-        while i < 25:
-            if current_touch_all & pin_bit:
-                if touch_ave > 0:
-                    while current_touch_all & pin_bit:
-                        # 2つめの判定
-                        touch_ave2 = touch_ave2 + i
-                        touch_ave_size2 = touch_ave_size2 + 1
-                        pin_bit = pin_bit << 1
-                        i = i + 1
-                        print("while", i)
+    #if touch1 and touch1 & before1:
+    if touch1:
 
-                else:
+        # サーチ開始
+        for i in range(13):
+            pin_bit = 1 << i
 
-                    while current_touch_all & pin_bit:
-                        # 1つめの判定
-                        touch_ave = touch_ave + i
-                        touch_ave_size = touch_ave_size + 1
-                        pin_bit = pin_bit << 1
-                        i = i + 1
-                        print("while", i)
+            if touch1 & pin_bit:
+                current1 = pin_bit
 
-                pin_bit = pin_bit >> 1
-                i = i - 1
+                # 前回タッチ位置が何もなかった場合
+                if not before1:
+                    before1 = pin_bit
+                    continue
 
-            pin_bit = pin_bit << 1
-            if touch_ave2 > 0:
-                i = 24
+                if not current1 & before1:
 
-            i = i + 1
+                    if current1 - before1 > 0:
+                        touch1_input = "touch1: <-"
+                        touch1_in = -1
+                        before1 = current1
 
-        if touch_ave_size != 0:
-            touch_ave = touch_ave / touch_ave_size
-        else:
-            touch_ave = 0
+                    elif current1 - before1 <= 0: 
+                        touch1_input = "touch1: ->"
+                        touch1_in = 1
+                        before1 = current1
 
-        if touch_ave_size2 != 0:
-            touch_ave2 = touch_ave2 / touch_ave_size2
-        else:
-            touch_ave2 = 0
-
-        # 前回判定チェック
-        if last_touch_ave > 0:
-            if touch_ave2 > 0 and touch_ave2 == 0:
-                if abs(last_touch_ave - touch_ave) > 5:
-                    last_touch_ave = last_touch_ave2
-                    last_touch_ave2 = 0
-
-            elif last_touch_ave2 == 0 and touch_ave2 > 0:
-                if abs(last_touch_ave - touch_ave) > 5:
-                    touch_ave = touch_ave2
-                    touch_ave2 = 0
-
-            if last_touch_ave > 0 and touch_ave:
-                IPpoint1 = last_touch_ave - touch_ave
-
-            if last_touch_ave2 > 0 and touch_ave2:
-                IPpoint2 - last_touch_ave2 - touch_ave2
-
-
-            if IPpoint1 > 0:
-                IPpoint1_vector = 1
-
-            elif IPpoint1 < 0:
-                IPpoint1_vector = -1
-
-            if IPpoint2 > 0:
-                IPpoint2_vector = 1
-
-            elif IPpoint2 < 0:
-                IPpoint2_vector = -1
-
-            now_input = last_input
-
-        print("IPPoint1:", IPpoint1, "IPpoint2:", IPpoint2)
-        print("IPPointvector", IPpoint1_vector)
-        print("IPPointvecto2r", IPpoint2_vector)
-        last_touch_ave = touch_ave
-        last_touch_ave2 = touch_ave2
+            '''
+            elif touch1 & pin_bit and pin_bit & before1:
+            elif touch1 & pin_bit and pin_bit & before1:
+                before1 = current1
+            '''
 
     else:
+        current1 = 0
+        touch1_in = 0
+        before1 = 0b0
+        touch1_input = "touch1: --"
 
-        last_touch_ave = touch_ave
-        last_touch_ave2 = touch_ave2
-        IPpoint1 = 0
-        IPpoint2 = 0
-        IPpoint1_vector = 0
-        IPpoint2_vector = 0
-        touch_ave_size = 0
-        touch_ave_size2 = 0
+    #if touch2 and touch2 & before2:
+    if touch2:
+        for i in range(13):
+            pin_bit = 1 << i
+
+            if touch2 & pin_bit and not pin_bit & before2:
+                current2 = pin_bit
+
+                # 前回タッチ位置が何もなかった場合
+                if not before2:
+                    before2 = pin_bit
+                    continue
+
+                if current2 - before2 > 0:
+                    touch2_input = "touch2: <-"
+                    touch2_in = -1
+                elif current2 - before2 < 0: 
+                    touch2_input = "touch2: ->"
+                    touch2_in = 1
+                before2 = current2
+                continue
+
+            elif touch2 & pin_bit and pin_bit & before2:
+                before2 = current2
+
+        before2 = current2
+
+    else:
+        current2 = 0
+        touch2_in = 0
+        before2 = 0b0
+        touch2_input = "touch2: --"
 
 
-    time.sleep(1)
+    if touch1_in == -1:
+        pi.digitalWrite(L1_PIN, pi.HIGH)
+    elif touch1_in == 1:
+        pi.digitalWrite(R1_PIN, pi.HIGH)
+    else:
+        pi.digitalWrite(L1_PIN, pi.LOW)
+        pi.digitalWrite(R1_PIN, pi.LOW)
 
+    if touch2_in == -1:
+        pi.digitalWrite(L2_PIN, pi.HIGH)
+    elif touch2_in == 1:
+        pi.digitalWrite(R2_PIN, pi.HIGH)
+    else:
+        pi.digitalWrite(L2_PIN, pi.LOW)
+        pi.digitalWrite(R2_PIN, pi.LOW)
 
-
-LED_PIN=14
-
-pi.wiringPiSetupGpio()
-pi.pinMode (LED_PIN, pi.OUTPUT)
-pi.digitalWrite( LED_PIN, pi.LOW)
+    
+    print("\r",format(touch1,"012b") ,format(current1,"012b") ,touch1_input, touch2_input, format(current2,"012b"), format(touch2,"012b"), end="")
+    #print(format(touch1,"012d") ,touch1_input, touch2_input, format(touch2,"012d"))
